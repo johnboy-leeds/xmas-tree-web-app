0d3b4f838a56fdf9efe96298ad0bb9e73f74f525
John <j.hickling@gmail.com>
2020-12-17 16:22:33 +0000

    disable while submitting, disable cors

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import './App.css';
+import { useState } from 'react';
 
 function App() {
-  const handleButtonClick = () => {
-    fetch('https://johnboy.ngrok.io/tree/demo', {method: 'POST'})
+  const [isMakingRequest, setMakingRequest] = useState(false)
+  const handleButtonClick = async () => {
+    setMakingRequest(true)
+    await fetch('https://johnboy.ngrok.io/tree/demo', {method: 'POST', mode: 'no-cors'})
+    setMakingRequest(false)
   }
 
   return (
     <div className="App">
       <header className="App-header">
-        <button onClick={handleButtonClick}>click me</button>
+        <button disabled={isMakingRequest} onClick={() => handleButtonClick()}>demo</button>
       </header>
     </div>
   );
